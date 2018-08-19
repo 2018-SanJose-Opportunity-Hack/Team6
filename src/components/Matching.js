3cbe7aede881c749aa289fa6f4eccc32955d3684
Emmanuel Vo <emmanuelvo777@gmail.com>
2018-08-19 00:53:32 -0700

    Implement filtering links

--- src/components/Matching.js
+++ src/components/Matching.js
@@ -20,7 +20,6 @@ export default class Matching extends React.Component {
   }
 
   onSchool(e) {
-    console.log('e', e.target.value);
     this.setState({ school: e.target.value });
   }
 
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+
 
 const Users = (props) => {
   const channels = props.data.map((user, i) => {
-    return (
-      <ul key={i}>{user.title}</ul>
-    );
+    if (user.colleges.indexOf(props.school) !== -1 ) {
+      return (
+        <div key={i}>
+          <Link to="/chat"><ul>{user.name}</ul></Link>
+        </div>
+      );
+    }
   });
 
   return (
